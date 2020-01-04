76bece9492c6abf3446853535ef45528904a5183
Sukjune Lee <robin00q@naver.com>
2020-01-04 18:30:35 +0900

    Add update function
    회원가입 기능 추가 완료

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -1,29 +1,67 @@
 package net.slipp.web;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import net.slipp.domain.User;
+import net.slipp.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
-	private List<User> users = new ArrayList<User>();
 	
-	@PostMapping("/create")
+	@Autowired
+	private UserRepository userRepository;
+	
+	@GetMapping("/form")
+	public String form() {
+		return "/user/form";
+	}
+	
+	@PostMapping("")
 	public String create(User user) {
 		System.out.println("user : " + user);
-		users.add(user);
+		userRepository.save(user);
 		
-		return "redirect:/list";
+		return "redirect:/users";
 	}
 	
-	@GetMapping("/list")
+	@GetMapping("")
 	public String list(Model model) {
-		model.addAttribute("users", users);
+		model.addAttribute("users", userRepository.findAll());
 		
-		return "list";
+		return "/user/list";
 	}
+	
+	@GetMapping("/{id}/form")
+	public String updateForm(@PathVariable Long id, Model model) {
+		User user = userRepository.findById(id).get();
+		model.addAttribute("user", user);
+		return "/user/updateform";
+	}
+	
+	@PostMapping("/{id}/update")
+	public String update(@PathVariable Long id, User newUser) {
+		User user = userRepository.findById(id).get();
+		user.update(newUser);
+		userRepository.save(user);
+
+		return "redirect:/users";
+	}
+	
+//	@PostMapping("/{id}/update")
+//	public String update(@PathVariable Long id, User newUser) {
+//		User user = userRepository.findById(id).get();
+//		user.update(newUser);
+//		userRepository.save(user);
+//
+//		return "redirect:/users";
+//	}
 }
