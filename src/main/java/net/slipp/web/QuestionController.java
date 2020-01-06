12063a33bb170b14f73d03713bbc068ee8697b4f
Sukjune Lee <robin00q@naver.com>
2020-01-06 19:04:11 +0900

    질문 수정, 삭제 기능 구현

--- src/main/java/net/slipp/web/QuestionController.java
+++ src/main/java/net/slipp/web/QuestionController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -50,4 +51,26 @@ public class QuestionController {
 		return "qna/show";
 	}
 	
+	@GetMapping("/{id}/form")
+	public String updateform(@PathVariable Long id, Model model) {
+		model.addAttribute("question", questionRepository.findById(id).get());
+		
+		return "qna/updateForm";
+	}
+	
+	@PostMapping("/{id}")
+	public String update(@PathVariable Long id, String title, String contents) {
+		Question question = questionRepository.findById(id).get();
+		question.update(title, contents);
+		questionRepository.save(question);
+		
+		return String.format("redirect:/questions/%d", id);
+	}
+	
+	@PostMapping("/{id}/delete")
+	public String delete(@PathVariable Long id, String title) {
+		questionRepository.deleteById(id);
+
+		return "redirect:/";
+	}
 }
